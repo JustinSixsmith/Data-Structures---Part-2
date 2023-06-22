3fea20182ad3f33c31554a94b52041f7e94a3b84
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-22 08:58:56 -0500

    Implements height method

--- DataStructures/src/start/main.java
+++ DataStructures/src/start/main.java
@@ -10,6 +10,6 @@ public class main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-        tree.traversePreOrder();
+        System.out.println(tree.height());
     }
 }
