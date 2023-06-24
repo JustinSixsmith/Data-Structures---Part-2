ceb8377fa9ce45f62c0bf0d2c43805b961ef956e
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-24 09:03:18 -0500

    Implements insert and balance of AVLTree

--- DataStructures/src/start/main.java
+++ DataStructures/src/start/main.java
@@ -2,14 +2,9 @@ package start;
 
 public class main {
     public static void main(String[] args) {
-        Tree tree = new Tree();
-        tree.insert(7);
-        tree.insert(4);
-        tree.insert(9);
-        tree.insert(1);
-        tree.insert(6);
-        tree.insert(8);
+        AVLTree tree = new AVLTree();
         tree.insert(10);
-        tree.traverseLevelOrder();
+        tree.insert(30);
+        tree.insert(20);
     }
 }
