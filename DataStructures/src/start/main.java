fa89872e0c553b105e636a95366fbe64b2e28de1
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-26 09:26:43 -0500

    Implements insert and bubbling up in heap

--- DataStructures/src/start/main.java
+++ DataStructures/src/start/main.java
@@ -2,9 +2,12 @@ package start;
 
 public class main {
     public static void main(String[] args) {
-        AVLTree tree = new AVLTree();
-        tree.insert(10);
-        tree.insert(30);
-        tree.insert(20);
+        var heap = new Heap();
+        heap.insert(10);
+        heap.insert(5);
+        heap.insert(17);
+        heap.insert(4);
+        heap.insert(22);
+        System.out.println("Done");
     }
 }
