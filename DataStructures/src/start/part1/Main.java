49be62de3df2a9390963ba7cf5490f03ddc728cf
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-26 11:15:49 -0500

    Implements priority queue using a heap

--- DataStructures/src/start/part1/Main.java
+++ DataStructures/src/start/part1/Main.java
@@ -2,6 +2,11 @@ package start.part1;
 
 public class Main {
     public static void main(String[] names) {
+        var queue = new PriorityQueue();
+        queue.add(30);
+        queue.add(20);
+        queue.add(10);
+        System.out.println(queue);
     }
 }
 
