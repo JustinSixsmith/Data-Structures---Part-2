63f594d792403abf39de6b69034340707b5cd475
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-26 11:33:01 -0500

    Heapifies an array

--- DataStructures/src/start/part2/main.java
+++ DataStructures/src/start/part2/main.java
@@ -4,12 +4,8 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
-        int[] numbers = {5, 3, 10, 1, 4, 2};
-        var heap = new Heap();
-        for (var number : numbers)
-            heap.insert(number);
-        for (int i = numbers.length - 1; i >= 0; i--)
-            numbers[i] = heap.remove();
+        int[] numbers = {5, 3, 8, 4, 1, 2};
+        MaxHeap.heapify(numbers);
         System.out.println(Arrays.toString(numbers));
     }
 }
