db6532716a061fe597f93d47a072d4a958d35343
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-26 22:43:11 -0500

    Search for a word in a Trie

--- DataStructures/src/start/part2/main.java
+++ DataStructures/src/start/part2/main.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class main {
     public static void main(String[] args) {
         var trie = new Trie();
-        trie.insert("cat");
-        trie.insert("can");
-        System.out.println("Done");
+        trie.insert("canada");
+        System.out.println(trie.contains(""));
     }
 }
