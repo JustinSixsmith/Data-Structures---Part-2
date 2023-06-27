d13f43fd58323520e6408634c6b55c3833853ac6
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-27 09:13:59 -0500

    Implements auto-completion

--- DataStructures/src/start/part2/main.java
+++ DataStructures/src/start/part2/main.java
@@ -6,9 +6,11 @@ public class main {
     public static void main(String[] args) {
         var trie = new Trie();
         trie.insert("car");
+        trie.insert("card");
         trie.insert("care");
-        trie.remove("book");
-        System.out.println(trie.contains("car"));
-        System.out.println(trie.contains("care"));
+        trie.insert("careful");
+        trie.insert("egg");
+        var words = trie.findWords(null);
+        System.out.println(words);
     }
 }
