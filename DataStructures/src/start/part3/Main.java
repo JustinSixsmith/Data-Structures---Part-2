5c4a2b190b82d91f9e9ef70b823ac576a155e5cb
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-06 18:03:25 -0500

    Implements BubbleSort

--- DataStructures/src/start/part3/Main.java
+++ DataStructures/src/start/part3/Main.java
@@ -1,7 +1,12 @@
 package start.part3;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[] numbers = {7, 3, 1, 4, 6, 2, 3};
+        var sorter = new BubbleSort();
+        sorter.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
