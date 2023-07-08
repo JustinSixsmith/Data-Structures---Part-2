e60c9771e45164a0cf63872beb46be41562defe7
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-08 09:25:27 -0500

    Implements CountingSort

--- DataStructures/src/start/part3/Main.java
+++ DataStructures/src/start/part3/Main.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {7, 3, 1, 5, 2};
-        var sorter = new QuickSort();
-        sorter.sort(numbers);
+        int[] numbers = {5, 1, 7, 2, 6, 4};
+        var sorter = new CountingSort();
+        sorter.sort(numbers, 7);
         System.out.println(Arrays.toString(numbers));
     }
 }
