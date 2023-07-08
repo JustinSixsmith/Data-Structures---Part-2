fe838e8433324be7248173676a7039abe6dc1144
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-08 10:12:34 -0500

    Implements BucketSort

--- DataStructures/src/start/part3/CountingSort.java
+++ DataStructures/src/start/part3/CountingSort.java
@@ -1,10 +1,13 @@
 package start.part3;
 
+import java.util.Arrays;
+
 public class CountingSort {
     public void sort(int[] array, int max) {
         int[] counts = new int[max + 1];
         for (var item : array)
             counts[item]++;
+        System.out.println(Arrays.toString(counts));
 
         var k = 0;
         for (int i = 0; i < counts.length; i++) {
--- DataStructures/src/start/part3/Main.java
+++ DataStructures/src/start/part3/Main.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {5, 1, 7, 2, 6, 4};
-        var sorter = new CountingSort();
-        sorter.sort(numbers, 7);
+        int[] numbers = {7, 1, 3, 5, 3};
+        var sorter = new BucketSort();
+        sorter.sort(numbers, 3);
         System.out.println(Arrays.toString(numbers));
     }
 }
