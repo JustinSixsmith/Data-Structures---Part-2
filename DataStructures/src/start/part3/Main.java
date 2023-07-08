3cb53f440a98e757f8f588b7c05fd00583973eaf
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-08 12:36:01 -0500

    Implements recursive binary search

--- DataStructures/src/start/part3/Main.java
+++ DataStructures/src/start/part3/Main.java
@@ -1,12 +1,13 @@
 package start.part3;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {7, 1, 3, 6, 5};
+        int[] numbers = {1, 3, 5, 6, 7};
         var search = new Search();
-        var index = search.linearSearch(numbers, 70);
+        var index = search.binarySearchRec(numbers, 1);
         System.out.println(index);
     }
 }
--- DataStructures/src/start/part3/Search.java
+++ DataStructures/src/start/part3/Search.java
@@ -1,13 +1,32 @@
 package start.part3;
 
 public class Search {
-    public int linearSearch(int[] array, int targetValue) {
+    public int linearSearch(int[] array, int target) {
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == targetValue)
+            if (array[i] == target)
                 return i;
         }
         return -1;
     }
 
+    public int binarySearchRec(int[] array, int target) {
+        return binarySearchRec(array, target, 0, array.length - 1);
+    }
 
+    private int binarySearchRec(
+            int[] array, int target, int left, int right) {
+
+        if (right < left)
+            return -1;
+
+        int middle = (left + right) / 2;
+
+        if (array[middle] == target)
+            return middle;
+
+        if (target < array[middle])
+            return binarySearchRec(array, target, left, middle - 1);
+
+        return binarySearchRec(array, target, middle + 1, right);
+    }
 }
