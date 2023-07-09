eeb915b65bf90e1c3a92f4d8fcd9fe9b5faadaf5
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-08 21:06:31 -0500

    Implements ternary search

--- DataStructures/src/start/part3/Main.java
+++ DataStructures/src/start/part3/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 7};
         var search = new Search();
-        var index = search.binarySearch(numbers, 70);
+        var index = search.ternarySearch(numbers, 7);
         System.out.println(index);
     }
 }
--- DataStructures/src/start/part3/Search.java
+++ DataStructures/src/start/part3/Search.java
@@ -48,4 +48,33 @@ public class Search {
 
         return -1;
     }
+
+    public int ternarySearch(int[] array, int target) {
+        return ternarySearch(array, target, 0, array.length - 1);
+    }
+
+    private int ternarySearch(int[] array, int target, int left, int right) {
+        if (left > right)
+            return -1;
+
+        int partitionSize = (right - left) / 3;
+        var mid1 = left + partitionSize;
+        var mid2 = right - partitionSize;
+
+        if (array[mid1] == target)
+            return mid1;
+
+        if (array[mid2] == target)
+            return mid2;
+
+        if (target < array[mid1])
+            return ternarySearch(array, target, left, mid1 - 1);
+
+        if (target > array[mid2])
+            return ternarySearch(array, target, mid2 + 1, right);
+
+        return ternarySearch(array, target, mid1 + 1, mid2 - 1);
+    }
+
+
 }
