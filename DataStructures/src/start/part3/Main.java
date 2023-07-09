975161e8f7f18c990510926726d10865397a60c4
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-09 18:56:06 -0500

    Implements exponential search

--- DataStructures/src/start/part3/Main.java
+++ DataStructures/src/start/part3/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 7};
         var search = new Search();
-        var index = search.jumpSearch(numbers, 5);
+        var index = search.exponentialSearch(numbers, 1);
         System.out.println(index);
     }
 }
