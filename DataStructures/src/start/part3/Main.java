3bfc0725823f99056442a91c5e82749d02d09b51
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-10 09:28:40 -0500

    Finds anagrams

--- DataStructures/src/start/part3/Main.java
+++ DataStructures/src/start/part3/Main.java
@@ -2,7 +2,7 @@ package start.part3;
 
 public class Main {
     public static void main(String[] args) {
-        var result = StringUtils.capitalize("   TODAY IS A GREAT DAY");
+        var result = StringUtils.areAnagrams("ABCD", "DBcA");
         System.out.println(result);
     }
 }
