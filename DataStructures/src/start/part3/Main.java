809caff749a1944ba416be15770c6c8d921a3025
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-10 09:17:22 -0500

    Capitalizes first letter in each word

--- DataStructures/src/start/part3/Main.java
+++ DataStructures/src/start/part3/Main.java
@@ -2,7 +2,7 @@ package start.part3;
 
 public class Main {
     public static void main(String[] args) {
-        var result = StringUtils.getMaxOccuringChar("Trees are beautiful");
+        var result = StringUtils.capitalize("   TODAY IS A GREAT DAY");
         System.out.println(result);
     }
 }
